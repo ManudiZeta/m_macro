c1e6025e08758f765b8d58952c4b84b9424902e9
Emanuele Zanusso <afteremanuele@gmail.com>
2025-10-29 10:24:05 +0100

    J/psi isr cn VECTORISR

--- isr/isr_gen.py
+++ isr/isr_gen.py
@@ -12,30 +12,35 @@ SM = ROOT.TStopwatch()
 SM.Start()
 
 N_ev = int(sys.argv[1]) #number of events
-choice = int(sys.argv[2]) # 0 = classic channel, 1 = J/psi channel
+choice = int(sys.argv[2]) # 0 = classic channel (phokhara evt_gen), 1 = J/psi channel (vector_ISR)
 
 # Create the steering path
 main = b2.Path()
 
-if choice == 0: 
-    print("****** CHANNEL e+ e- (isr) --> vpho --> p+ pi- nbar ****** ")
-    decfile = "../dec_file/dec_isr.dec"
-    final_state = ['p+', 'pi-', 'anti-n0']
-
-if choice == 1:  
-    print("****** CHANNEL e+ e- (isr) --> vpho --> J/psi --> p+ pi- nbar ****** ")
-    decfile = "../dec_file/dec_isr_jpsi.dec"
-    final_state = ['J/psi']
-
 # Define number of events and experiment number
 main.add_module('EventInfoSetter', evtNumList=[N_ev], expList=[0])
 
-ge.add_phokhara_evtgen_combination(path=main,
-final_state_particles = final_state, 
-user_decay_file = decfile,)
-#beam_energy_spread=True,  
-#isr_events=True)
-#min_inv_mass_vpho=2.1, max_inv_mass_vpho=10.6)
+if choice == 0: 
+    print("****** CHANNEL e+ e- (isr) --> vpho --> p+ pi- nbar ****** ")
+    final_state = ['p+', 'pi-', 'anti-n0']
+    decfile = "../dec_file/dec_isr.dec"
+    #Generate isr events with phokhara_evt_gen
+    ge.add_phokhara_evtgen_combination(path=main,
+    final_state_particles = final_state, 
+    user_decay_file = decfile,)
+    #beam_energy_spread=True,  
+    #isr_events=True)
+
+    #min_inv_mass_vpho=2.1, max_inv_mass_vpho=10.6)
+    
+
+if choice == 1:  
+    print("****** CHANNEL e+ e- (isr) --> vpho --> J/psi --> p+ pi- nbar ****** ")
+    final_state = ['J/psi']
+    decfile = "../dec_file/dec_isrVEC_jpsi.dec"
+    #Generate isr events with evt_gen_VECTORISR
+    ge.add_evtgen_generator(path=main,finalstate='signal',signaldecfile=b2.find_file(decfile))
+    
 
 # Simulate the detector response 
 si.add_simulation(path=main)
@@ -47,7 +52,8 @@ re.add_reconstruction(path=main)
 if choice == 0:
     mdst.add_mdst_output(path=main, filename='../../root_file/isr/isr_output_test_False.root')
 if choice == 1:
-    mdst.add_mdst_output(path=main, filename='../../root_file/isr/isr_output_Jpsi_test_False.root')
+    mdst.add_mdst_output(path=main, filename='../../root_file/isr/isrVEC_output_Jpsi.root')
+
 # Process the steering path
 b2.process(path=main)
 
--- isr/isr_head.py
+++ isr/isr_head.py
@@ -8,31 +8,27 @@ import sys
 from variables import variables as vm
 from variables.MCGenTopo import mc_gen_topo
 
-choice = int(sys.argv[1]) # 0 = with photon, otherwise = without photon
+choice = int(sys.argv[1]) # 0 = original channel, 1 = J/psi channel
 
 main = b2.Path()
-ma.inputMdstList(filelist=["../../root_file/isr/isr_output_False.root"],path=main)
+if choice == 0: 
+    ma.inputMdstList(filelist=["../../root_file/isr/isr_output.root"],path=main)
+if choice == 1:  
+    ma.inputMdstList(filelist=["../../root_file/isr/isr_output_Jpsi_test_False.root"],path=main)
 
 lista = "REC"
 
 # Ricostruzione delle particelle visibili
 ma.fillParticleList(f"p+:{lista}", "", path=main) 
 ma.fillParticleList(f"pi-:{lista}", "", path=main)
+ma.fillParticleList(f"gamma:{lista}", "", path=main) 
 ma.fillParticleList(f"anti-n0:{lista}", "", path=main)
 
-if choice == 0:
-    title = "gamma"
-    print("*** ISR events WITH gamma in RecDecay *** \n")
-
-    ma.fillParticleList(f"gamma:{lista}", "", path=main) # "tutti" because "all" is protected from further cuts
-    ma.reconstructDecay(f"vpho:list_rec -> p+:{lista} pi-:{lista} gamma:{lista}",cut="",path=main)
-else:
-    title = "no_gamma"
-    print("*** ISR events WITHOUT gamma in RecDecay *** \n")
-
-    ma.reconstructDecay(f"vpho:list_rec -> p+:{lista} pi-:{lista}",cut="",path=main)
-
+ma.reconstructDecay(f"vpho:list_rec -> p+:{lista} pi-:{lista} gamma:{lista}",cut="",path=main)
 ma.reconstructDecay(f"vpho:gen -> vpho:list_rec anti-n0:{lista} ",cut="",path=main)
+
+if choice == 1:
+    ma.reconstructDecay(f" J/psi:list_rec -> p+:{lista} pi-:{lista} anti-n0:{lista}",cut="",path=main)
 
 ma.matchMCTruth("vpho:gen", path=main)
 
@@ -41,48 +37,55 @@ g_vars = vc.kinematics + vc.mc_kinematics + ['mcISR', 'mcPDG', 'genMotherPDG','p
 
 b_vars = vc.kinematics + vc.mc_kinematics + ['isSignal'] + vc.recoil_kinematics
 
-daug_vars = ['isSignal','PDG','mcErrors', 'mcPDG', 'mcFSR', 'genMotherPDG','genMotherID','clusterNHits','clusterLAT','clusterE1E9','clusterAbsZernikeMoment40','clusterAbsZernikeMoment51','clusterE9E21','isFromECL','isFromTrack','M','p','E','phi','theta','mcPhi','mcTheta','mcP','mcE', 'clusterE','clusterUncorrE']
+daug_vars = ['isSignal','PDG','mcErrors', 'mcPDG', 'mcISR','mcFSR', 'genMotherPDG','genMotherID','clusterNHits','clusterLAT','clusterE1E9','clusterAbsZernikeMoment40','clusterAbsZernikeMoment51','clusterE9E21','isFromECL','isFromTrack','M','p','E','phi','theta','mcPhi','mcTheta','mcP','mcE', 'clusterE','clusterUncorrE']
 
-if choice == 0: 
-    b_vars = b_vars + vu.create_aliases_for_selected(daug_vars, "vpho:gen -> [vpho:list_rec -> ^p+ ^pi- ^gamma] ^anti-n0", prefix = ["p", "pi", "gamma", "nbar"])
+b_vars = vu.create_aliases_for_selected(daug_vars, "^vpho:gen -> [^vpho:list_rec -> ^p+ ^pi- ^gamma] ^anti-n0", prefix = ["vpho_g","vpho_r","p", "pi", "gamma", "nbar"])
 
-    b_vars = b_vars + vu.create_aliases_for_selected(vc.recoil_kinematics, "vpho:gen -> [^vpho:list_rec -> p+ pi- gamma] anti-n0", prefix = ["vpho"])
+if choice == 1:
+    b_vars = b_vars + vu.create_aliases_for_selected(daug_vars, "^J/psi -> p+ pi- anti-n0", prefix = ["JPsi"])
 
-    b_vars = b_vars + vu.create_aliases_for_selected(g_vars, "vpho:gen -> [vpho:list_rec -> p+ pi- ^gamma] anti-n0", prefix = ["gamma"])
+b_vars = b_vars + vu.create_aliases_for_selected(vc.recoil_kinematics, "vpho:gen -> [^vpho:list_rec -> p+ pi- gamma] anti-n0", prefix = ["vpho_r"])
 
-else:
-    b_vars = b_vars + vu.create_aliases_for_selected(daug_vars, "vpho:gen -> [vpho:list_rec -> ^p+ ^pi-] ^anti-n0", prefix = ["p", "pi", "nbar"])
 
-    b_vars = b_vars + vu.create_aliases_for_selected(vc.recoil_kinematics, "vpho:gen -> [^vpho:list_rec -> p+ pi-] anti-n0", prefix = ["vpho"])
-
-vm.addAlias("fir_arg","formula(sin(vpho_pRecoilTheta)*sin(nbar_theta)*cos(vpho_pRecoilPhi-nbar_phi))")
-vm.addAlias("sec_arg","formula(cos(vpho_pRecoilTheta)*cos(nbar_theta))")
+#Personal variable alpha
+vm.addAlias("fir_arg","formula(sin(vpho_r_pRecoilTheta)*sin(nbar_theta)*cos(vpho_r_pRecoilPhi-nbar_phi))")
+vm.addAlias("sec_arg","formula(cos(vpho_r_pRecoilTheta)*cos(nbar_theta))")
 vm.addAlias("alpha","formula(acos(fir_arg + sec_arg))")
-
 ma.rankByLowest("vpho:gen", "alpha", numBest=1, path=main)
-
 b_vars = b_vars + ['alpha']
 
+#Personal variable nbarE_buona
+vm.addAlias("numE","formula(nbar_clusterE*nbar_clusterE - 2*nbar_M*nbar_M)")
+vm.addAlias("denE","formula(2*nbar_M)")
+vm.addAlias("nbarE_buona","formula(numE/denE)")
+b_vars = b_vars +['nbarE_buona']
 
 #cmskinematics = vu.create_aliases(vc.kinematics + ['InvM','mRecoil','eRecoil'], "useCMSFrame({variable})", "CMS")
 #b_vars = b_vars + cmskinematics
-#print(b_vars)
 
-if choice == 0: 
-    sig_cuts = "vpho_mRecoil>0 and p_mcPDG == 2212 and pi_mcPDG == -211 and gamma_mcPDG == 22" 
+print(b_vars)
+
+sig_cuts = "vpho_r_mRecoil>0 and p_mcPDG == 2212 and pi_mcPDG == -211 and gamma_mcPDG == 22 and alpha < 0.35" 
+
+if choice == 0:
+    dad_cuts = "p_genMotherPDG == 10022 and pi_genMotherPDG == 10022"
+    #n0_cuts = "nbar_genMotherPDG == 300553"
+
 else:
-    sig_cuts = "vpho_mRecoil>0 and p_mcPDG == 2212 and pi_mcPDG == -211" 
+    dad_cuts = "p_genMotherPDG == 443 and pi_genMotherPDG == 443" #è giusto o provengono da vpho (10022) a livello di generatore? giusto 443, infatti non vi sono entries per 10022 quando non applico i tagli
+    #n0_cuts = "nbar_genMotherPDG == 443" #stesso discorso qui
 
-dad_cuts = "p_genMotherPDG == 10022 and pi_genMotherPDG == 10022"
-#n0_cuts = "nbar_genMotherPDG == 10022"
 cuts= sig_cuts + " and " + dad_cuts 
-#print(" *** ", cuts, " *** ")
+print(" *** ", cuts, " *** ")
+#ma.applyCuts("vpho:gen", cuts, path=main)
 
-ma.applyCuts("vpho:gen", cuts, path=main)
+if choice == 0:
+    ma.variablesToNtuple("vpho:gen",variables=b_vars,filename=f"../../root_file/isr/vpho_isr_{lista}_test.root",treename="tree",path=main,)
+    ma.variablesToNtuple("vpho:gen",variables=mc_gen_topo(200),filename=f"../../root_file/isr/isr_TOPO/vpho_isr_{lista}_test_TOPO.root",treename="tree",path=main,)
 
-
-ma.variablesToNtuple("vpho:gen",variables=b_vars,filename=f"../../root_file/isr/vpho_isr_{title}_{lista}_False.root",treename="tree",path=main,)
-ma.variablesToNtuple("vpho:gen",variables=mc_gen_topo(200),filename=f"../../root_file/isr/isr_TOPO/vpho_isr_{title}_{lista}_False_TOPO.root",treename="tree",path=main,)
+else:
+    ma.variablesToNtuple("vpho:gen",variables=b_vars,filename=f"../../root_file/isr/vpho_Jpsi_isr_{lista}_test.root",treename="tree",path=main,)
+    ma.variablesToNtuple("vpho:gen",variables=mc_gen_topo(200),filename=f"../../root_file/isr/isr_TOPO/vpho_Jpsi_isr_{lista}_test_TOPO.root",treename="tree",path=main,)
 
 b2.process(main)
 
